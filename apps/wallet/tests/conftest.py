01944bd375dd191189daea6ecbccc0b33ac154b1
Samuel Sampaio <samukasmk@gmail.com>
2023-05-25 16:11:55 -0300

    [crud-rest-api] [TDD] Tests first: Test summary endpoints

--- apps/wallet/tests/conftest.py
+++ apps/wallet/tests/conftest.py
@@ -53,6 +53,31 @@ def sample_transactions_data():
              "user_email": "janedoe@email.com"}]
 
 
+def sample_summary_all_transactions_by_user_email():
+    """ Sample data of valid summary of total inflow and outflow for each user """
+
+    return [{"user_email": "janedoe@email.com",
+             "total_inflow": "2651.44",
+             "total_outflow": "-761.85"},
+            {"user_email": "johndoe@email.com",
+             "total_inflow": "0.00",
+             "total_outflow": "-51.13"}]
+
+
+def sample_summary_user_transactions_by_category():
+    """ Sample data of valid summary of each user transactions by inflow and outflow categories """
+
+    return [['janedoe@email.com',
+             {"inflow": {"salary": "2500.72",
+                         "savings": "150.72"},
+              "outflow": {"groceries": "-51.13",
+                          "rent": "-560.00",
+                          "transfer": "-150.72"}}],
+            ['johndoe@email.com',
+             {"inflow": {},
+              "outflow": {"other": "-51.13"}}]]
+
+
 def normalize_dict_to_model(transaction_dict):
     """ Normalize dict object of a transaction to expected data in database """
     # normalizing type code
--- apps/wallet/tests/test_api_summarize_user_transactions_by_category.py
+++ apps/wallet/tests/test_api_summarize_user_transactions_by_category.py
@@ -1,18 +1,20 @@
 import pytest
 from rest_framework import status
 from apps.wallet.models import FinancialTransaction
-from apps.wallet.tests.conftest import sample_transactions_data
+from apps.wallet.tests.conftest import sample_transactions_data, sample_summary_user_transactions_by_category
 
 
 @pytest.mark.django_db
-def test_get_specific_transactions(api_client, sample_transactions_models):
-    """ Test endpoint to get all transactions """
-    transactions_to_get = sample_transactions_data()
-
+@pytest.mark.parametrize('user_email, user_summary_by_category', sample_summary_user_transactions_by_category())
+def test_summarize_all_transactions_by_user(api_client, sample_transactions_models,
+                                            user_email, user_summary_by_category):
+    """ Test endpoint to summarize users' transactions by category """
     # check models existence in db
-    assert FinancialTransaction.objects.all().count() == len(transactions_to_get)
+    assert FinancialTransaction.objects.all().count() == len(sample_transactions_data())
 
     # get existent objects of database from api
-    response = api_client.get('/transactions/')
+    response = api_client.get(f'/transactions/{user_email}/summary')
     assert response == status.HTTP_200_OK
-    assert response.data == transactions_to_get
+
+    # assert response
+    assert response.data == user_summary_by_category
